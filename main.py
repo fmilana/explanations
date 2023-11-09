384393eb1ee39d6f8a64b2cec7d9a2f41797b452
k1502432 <k1502432@kcl.ac.uk>
2023-11-09 15:40:46 +0000

    add comments for lime library implementation

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.pipeline import make_pipeline
 from eli5 import format_as_text, format_as_html
 from eli5.lime import TextExplainer
+from lime.lime_text import LimeTextExplainer
 from xgboost import XGBClassifier
 
 
@@ -102,11 +103,13 @@ pipeline.fit(X_train, Y_train)
 categories = ["food and drinks", "place", "people", "opinions"]
 
 # FOOD AND DRINKS
-# sentence = "They will even make you a burger in which the bun has been substituted for two halves of an avocado."
-sentence = "To the right, in a small parade, there's Neat Burger, knocking out pea protein and corn-based patties, dyed what they think are the right colours by the addition of beetroot and turmeric."
-# sentence = "But the word-spaghetti of the menu descriptions – 'aubergine, kalamatas, tahini' reads one; 'grilled beetroot, spring onion, smoked soy' reads another – is so alluring, so well lubricated with promise, that I give myself to it happily."
+sentence = "They will even make you a burger in which the bun has been substituted for two halves of an avocado."
+# sentence = "But the word-spaghetti of the menu descriptions – 'aubergine, kalamatas, tahini' reads one; \
+# 'grilled beetroot, spring onion, smoked soy' reads another – is so alluring, so well lubricated with promise, that I give myself to it happily."
 
 # PLACE
+# sentence = "To the right, in a small parade, there's Neat Burger, knocking out pea protein and corn-based patties, \
+#     dyed what they think are the right colours by the addition of beetroot and turmeric."
 # sentence = "Now, the upstairs dining room has parquet floors, comfortable midcentury modern tan leather chairs and a kitchen with principles."
 # sentence = "But the most interesting of these three restaurants on Princes Street, tucked in together for comfort, is in the middle."
 # sentence = "Tendril started as a pop-up, first in a Soho pub, then later here, on this narrow site just south of Oxford Street."
@@ -114,7 +117,9 @@ sentence = "To the right, in a small parade, there's Neat Burger, knocking out p
 # PEOPLE
 # sentence = "Head chef Graham Chatham, who has cooked at Rules and Daylesford Organic, treats them with old school care, attention and at times, maternal indulgence."
 # sentence = "The service is sometimes chaotic but, like a primary school ballet class, always enthusiastic."
-# sentence = "That's exactly what you would expect of a chef like Shaun Moffat, who has cooked in London at the Middle Eastern-inflected Berber & Q, and at the cheerfully iconoclastic Manteca, which treats the Italian classics as a mere opening position in a ribald negotiation."
+# sentence = "That's exactly what you would expect of a chef like Shaun Moffat, who has cooked in London at the \
+# Middle Eastern-inflected Berber & Q, and at the cheerfully iconoclastic Manteca, which treats the Italian \
+# classics as a mere opening position in a ribald negotiation."
 
 # OPINIONS
 # sentence = "There's an awful lot going on here."
@@ -129,8 +134,31 @@ print("cleared explanations.txt and explanations.html")
 
 n_samples_list = [300, 1000, 2000, 3000, 4000, 5000, 10000, 15000, 20000, 25000, 30000]
 
+# eli5
 for n_samples in n_samples_list:
     text_explainer = TextExplainer(random_state=42, n_samples=n_samples, position_dependent=True)
 
     print(f"calling explain_pred with n_samples={n_samples}")
     explain_pred(text_explainer, pipeline, categories, sentence)
+
+# lime
+# https://marcotcr.github.io/lime/tutorials/Lime%20-%20multiclass.html
+# print("Chains predictions:")
+# print(pipeline.predict_proba([sentence]))
+# print("=====================================")
+
+# explainer = LimeTextExplainer(class_names=categories)
+# print(f'len(sentence.split())={len(sentence.split())}')
+# exp = explainer.explain_instance(
+#     sentence, pipeline.predict_proba, 
+#     num_features=len(sentence.split()), 
+#     labels=[i for i, _ in enumerate(categories)]
+#     )
+
+# for i in range(len(categories)):
+#     print(f"Explanation for class {categories[i]}:")
+#     print("\n".join(map(str, exp.as_list(label=i))))
+#     print()
+
+# exp.save_to_file('results/lime.html')
+# debug: https://github.com/marcotcr/lime/issues/243
