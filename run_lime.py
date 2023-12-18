ef0bca59b4832e62ea5b6f131aba6bf7751d770a
fmilana <ucjufmi@ucl.ac.uk>
2023-12-18 14:27:02 +0100

    Add new sentences

--- run_lime.py
+++ run_lime.py
@@ -49,35 +49,61 @@ print("done calling pipeline.fit")
 categories = ["food and drinks", "place", "people", "opinions"]
 
 # FOOD AND DRINKS
+# TP
 # sentence = "They will even make you a burger in which the bun has been substituted for two halves of an avocado."
 # sentence = "But the word-spaghetti of the menu descriptions – 'aubergine, kalamatas, tahini' reads one; \
 # 'grilled beetroot, spring onion, smoked soy' reads another – is so alluring, so well lubricated with promise, that I give myself to it happily."
+# sentence = "Follow that from the specials with thickly glazed duck hearts grilled so they still have bite, then dotted with yellow splodges of nose-tickling mustard."
+# sentence = "It is difficult to get the balance of floral, sweet and acidity right in a herb sorbet."
+# FP
+# sentence = "Drink orders are forgotten, then have to be re-explained."
+# sentence = "At his first London restaurant Hibiscus, amid all the gastronomic knife juggling, he offered a sausage roll."
+# TN
+# FN
+# sentence = "It's a tough, extremely indifferent piece of meat."
+# sentence = "Bizarrely, they've removed the skin, which is an offence against the vengeful gods of fried chicken."
 
 # PLACE
+# TP
 # sentence = "To the right, in a small parade, there's Neat Burger, knocking out pea protein and corn-based patties, \
 # #     dyed what they think are the right colours by the addition of beetroot and turmeric."
 # sentence = "Now, the upstairs dining room has parquet floors, comfortable midcentury modern tan leather chairs and a kitchen with principles."
 # sentence = "But the most interesting of these three restaurants on Princes Street, tucked in together for comfort, is in the middle."
 # sentence = "Tendril started as a pop-up, first in a Soho pub, then later here, on this narrow site just south of Oxford Street."
+# FP
+# sentence = "Australian-born Lloyd Morse, who previously cooked at London's Spring, and Edinburgh native James Snowdon who was formerly the general manager of Fulham's Harwood Arms"
+# TN
+# FN
+# sentence = "It is an elbows-on-the-table sort of place, where you could hunker down by yourself over a bowl of something steaming and delicious that makes the world better, and all at an extremely good price."
 
 # PEOPLE
+# TP
 # sentence = "Head chef Graham Chatham, who has cooked at Rules and Daylesford Organic, treats them with old school care, attention and at times, maternal indulgence."
 # sentence = "The service is sometimes chaotic but, like a primary school ballet class, always enthusiastic."
 # sentence = "That's exactly what you would expect of a chef like Shaun Moffat, who has cooked in London at the \
 # Middle Eastern-inflected Berber & Q, and at the cheerfully iconoclastic Manteca, which treats the Italian \
 # classics as a mere opening position in a ribald negotiation."
+# sentence = "That might explain why we are brought a beetroot dish we didn't ask for."
+# FP
+# sentence = "But they were great, so really I'm not complaining."
+# TN
+# FN
+# sentence = "For Manchester's Chinese students, this small Hong Kong-style café is a true taste of home"
 
 # OPINIONS
+# TP
+# sentence = "Describing one part of the world's food traditions as intrinsically better than another's is just not the done thing."
+# sentence = "But it was strange and uneven rather than the perfect it should be."
 # sentence = "There's an awful lot going on here."
 # sentence = "It's restless but focused and jolly."
 # sentence = "Just go elsewhere afterwards for an ice-cream."
 # sentence = "Importantly though, it is good value."
-
-# food and drinks false positives
-sentence = "Drink orders are forgotten, then have to be re-explained."
-
-# food and drinks false negatives
-sentence = "It's a tough, extremely indifferent piece of meat."
+# FP
+# sentence = "My companion is much happier with their thick, slurpy, hand-pulled noodles."
+# TN
+# FN
+# sentence = "Modern food preparation technology is to be celebrated: it's cleaner, more energy efficient and just simpler to use than the old ways."
+sentence = "This is not cooking that redefines the very notion of Greek food."
 
 prediction = pipeline.predict([sentence]).flatten()
 try:
@@ -104,11 +130,11 @@ n_samples_list = [
     3000,
     4000,
     5000,
-    10000,
-    15000,
-    20000,
-    25000,
-    30000
+    # 10000,
+    # 15000,
+    # 20000,
+    # 25000,
+    # 30000
     ]
 
 # eli5 (lime)
