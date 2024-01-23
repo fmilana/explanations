1b4751886dfa0b2daf4c52c89e4840506232678d
fmilana <ucjufmi@ucl.ac.uk>
2024-01-23 12:44:03 +0000

    push split restaurant reviews

---
+++ train.py
@@ -0,0 +1,7 @@
+from pandas import pd
+from classifier import MultiLabelProbClassifier
+
+train_df = pd.read_csv("data/train.csv")
+
+# finish coding all 30 reviews:
+#/Users/fmilana/Documents/tacodingassistant/data/studies/restaurant_reviews/restaurant_reviews.docx
