0ab678c8fefc2be3f40563c7569ad5be13586594
fmilana <ucjufmi@ucl.ac.uk>
2024-02-12 13:31:06 +0000

    don't average occlusion probability since classifierchain already gives an average proba

--- occlusion_explain.py
+++ occlusion_explain.py
@@ -9,17 +9,9 @@ def get_occlusion_weights(pipeline, class_names, sentence, class_name, proba):
     for word in words:
         occluded_sentence = " ".join([w if w != word else "" for w in words])
 
-        occluded_probas = []
-
-        # average 10 predictions with occluded word
-        for _ in range(10):
-            occluded_proba  = pipeline.predict_proba([occluded_sentence])[class_names.index(class_name)]
-            occluded_probas.append(occluded_proba)
-        
-        average_occluded_proba = sum(occluded_probas) / len(occluded_probas)
-
+        occluded_proba  = pipeline.predict_proba([occluded_sentence])[class_names.index(class_name)]
         # convert numpy array to float
-        occlusion_weight = float(proba - average_occluded_proba)
+        occlusion_weight = float(proba - occluded_proba)
 
         occlusion_weights.append(occlusion_weight)
 
